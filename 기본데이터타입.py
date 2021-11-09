7ce5a93e0cfd6661ab520f4fbc9d6ffcd3f040bf
MOSE <triangle_so@naver.com>
2021-11-09 16:02:33 +0900

    2021.11.9

---
+++ 기본데이터타입.py
@@ -0,0 +1,24 @@
+# 파이썬 기본 데이터 타입
+a = 7
+b = 7.777
+c = True
+d = False
+e = "문자열"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(2/4)
+print(type(2/4))
+
+# 제곱과 나누기
+print(2**3) #2*2*2
+print(5//2) # 2 몫
+print(5%2) # 1 나머지
+
+# 반올림 round()
+print (round(3.156, 2)) #소수둘째자리까지 반올림
+
+a = input
