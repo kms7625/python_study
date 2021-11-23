298641167f22b7a4b61bb6f0c20b937d5e354067
MOSE <triangle_so@naver.com>
2021-11-23 16:26:56 +0900

    2021.11.23.화요일

---
+++ 😜git연결😜.py
@@ -0,0 +1,21 @@
+# -해당폴더 오른쪽마우스 git bash here클릭
+# -명령창에 git init입력
+# -git add . 입력(최초한번저장)
+# -git commet -m "폴더이름"
+# -git사이트 설정들어가서 폴더경로설정
+# -그다음페이지에서 주소 복사해서 명령창에 붙여넣기
+#   ex) https://github.com/[깃허브이름]/[폴더이름].git
+# git push -u origin master 입력
+# -git remote입력 후 origin뜨는지 확인
+# 깃허브 홈페이지들어가서 업로드됐는지 확인
+# 세팅들어가서 호스팅 주소 확인
+
+
+
+# [깃허브 삭제방법]
+
+# [깃만삭제하기]
+# rm -rf .gif
+
+# 당분간 자주볼듯..
+
