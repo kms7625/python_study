7bcd597057ef355332e1a55ffe62ad738f5237ce
MOSE <triangle_so@naver.com>
2021-11-23 15:51:52 +0900

    2021.11.23.화요일

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import hello
 
 # 불러온 헬로우 모듈의 __name__은 모듈의 이름과 같다.
 
-print('main.py __name__ : ', __name__) #__name__변수 출력
+print('main.py __name__ : ', __name__)  # __name__변수 출력
 
 # __main__이라는 값은 현재 실행되는 파일의 __name__변수 값이다.
